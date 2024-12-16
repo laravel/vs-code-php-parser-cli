91c207feda5272e781a0e1f223579abffcc9ed74
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2024-12-16 16:00:12 -0400

    fix: disable logging of deprecation warnings

--- config/logging.php
+++ config/logging.php
@@ -32,7 +32,7 @@ return [
     */
 
     'deprecations' => [
-        'channel' => env('LOG_DEPRECATIONS_CHANNEL', 'single'),
+        'channel' => env('LOG_DEPRECATIONS_CHANNEL', 'null'),
         'trace'   => env('LOG_DEPRECATIONS_TRACE', false),
     ],
 
