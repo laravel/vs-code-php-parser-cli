4f584e657bb71be9f7a0489cf916b0959be110b0
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2024-12-18 20:52:06 -0500

    wip

--- app/Commands/DetectCommand.php
+++ app/Commands/DetectCommand.php
@@ -8,38 +8,38 @@ use LaravelZero\Framework\Commands\Command;
 
 class DetectCommand extends Command
 {
-    protected $signature = 'detect {path} {--debug} {--from-file=}';
+    protected $signature = 'detect {code} {--debug} {--from-file=}';
 
     protected $description = 'Detect things we care about in the current code';
 
     public function handle(): void
     {
+        $code = base64_decode($this->argument('code'));
+
         if ($this->option('from-file')) {
             $code = file_get_contents(__DIR__ . '/../../tests/snippets/detect/' . $this->option('from-file') . '.php');
-        } else {
-            try {
-                $code = file_get_contents($this->argument('path'));
-            } catch (\Exception $e) {
-                echo json_encode([]);
-                return;
-            }
         }
 
         $walker = new DetectWalker($code, (bool) $this->option('debug'));
         $result = $walker->walk();
 
         if (app()->isLocal()) {
-            $dir = 'local-results/detect';
-            File::ensureDirectoryExists(storage_path($dir));
-            $now = now()->format('Y-m-d-H-i-s');
-
-            File::put(storage_path("{$dir}/result-{$now}.json"), $result->toJson(JSON_PRETTY_PRINT));
-
-            if (!$this->option('from-file')) {
-                File::put(storage_path("{$dir}/result-{$now}.php"), $code);
-            }
+            $this->log($result, $code);
         }
 
         echo $result->toJson();
+    }
+
+    protected function log($result, $code)
+    {
+        $dir = 'local-results/detect';
+        File::ensureDirectoryExists(storage_path($dir));
+        $now = now()->format('Y-m-d-H-i-s');
+
+        File::put(storage_path("{$dir}/result-{$now}.json"), $result->toJson(JSON_PRETTY_PRINT));
+
+        if (!$this->option('from-file')) {
+            File::put(storage_path("{$dir}/result-{$now}.php"), $code);
+        }
     }
 }
