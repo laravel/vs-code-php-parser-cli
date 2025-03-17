f23df4b598fa618fddbb88e06e1b59bebb7607dc
N1ebieski <kontakt@intelekt.net.pl>
2025-03-17 11:37:42 +0000

    Add ArrayValue to searchForAutocompleting Fixes N1ebieski/vs-code-php-parser-cli#7

--- app/Parsers/ArrayCreationExpressionParser.php
+++ app/Parsers/ArrayCreationExpressionParser.php
@@ -5,7 +5,9 @@ namespace App\Parsers;
 use App\Contexts\AbstractContext;
 use App\Contexts\ArrayValue;
 use Microsoft\PhpParser\MissingToken;
+use Microsoft\PhpParser\Node;
 use Microsoft\PhpParser\Node\Expression\ArrayCreationExpression;
+use Microsoft\PhpParser\Node\Expression\CallExpression;
 
 class ArrayCreationExpressionParser extends AbstractParser
 {
@@ -14,9 +16,27 @@ class ArrayCreationExpressionParser extends AbstractParser
      */
     protected AbstractContext $context;
 
+    private function isParentNode(Node $node, string $nodeClass): bool
+    {
+        if ($node->getParent() !== null) {
+            if ($node->getParent() instanceof $nodeClass) {
+                return true;
+            }
+
+            return $this->isParentNode($node->getParent(), $nodeClass);
+        }
+
+        return false;
+    }
+
     public function parse(ArrayCreationExpression $node)
     {
-        $this->context->autocompleting = $node->closeParenOrBracket instanceof MissingToken;
+        // If array is inside a method, for example Validator::validate(['
+        // then we need to ignore autocompleting for ArrayValue because
+        // priority is given to App\Contexts\MethodCall
+        if (!$this->isParentNode($node, CallExpression::class)) {
+            $this->context->autocompleting = $node->closeParenOrBracket instanceof MissingToken;
+        }
 
         return $this->context;
     }
