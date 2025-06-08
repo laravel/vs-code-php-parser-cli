831df140adbcc798a21dbe9a15463c04b3e68553
N1ebieski <kontakt@intelekt.net.pl>
2025-06-08 10:06:30 +0000

    merge upstream main

--- app/Contexts/Variable.php
+++ app/Contexts/Variable.php
@@ -4,7 +4,7 @@ namespace App\Contexts;
 
 class Variable extends AbstractContext
 {
-    public bool $findable = true;
+    public bool $isAbleToAutocomplete = true;
 
     public ?string $name = null;
 
