71b2acb3d3bb5d2418d106d7a6fce200cebf3b6c
N1ebieski <kontakt@intelekt.net.pl>
2025-09-09 09:07:43 +0000

    fix name to VarName

--- app/Parsers/VariableParser.php
+++ app/Parsers/VariableParser.php
@@ -18,9 +18,9 @@ class VariableParser extends AbstractParser
 
     public function parse(Variable $node)
     {
-        $this->context->name = $node->getName();
+        $this->context->varName = $node->getName();
 
-        $result = $this->context->searchForVar($this->context->name);
+        $result = $this->context->searchForVar($this->context->varName);
 
         if (is_string($result)) {
             $this->context->className = $result;
